864e5dd913d7252c4ec21c7821ef4630cc38d1f0
Anani1994 <dongwhchn@163.com>
2018-07-10 19:39:30 +0800

    configure router & install webpack-dev-server & update READE.md

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue';
 import App from './app.vue';
-
+import { router } from './router/index';
 
 new Vue({ // 创建 vue 实例
-  el: '#app', // 提供一个在页面上已经存在的 DOM 元素作为 Vue 实例挂载目标
-  render: h => h(App) // 声明了 html 中的内容
+  // 提供一个在页面上已经存在的 DOM 元素作为 Vue 实例挂载目标
+  el: '#app', 
+  // 创建和挂载根实例
+  router: router,
+  // 声明了 html 中的内容
+  render: h => h(App)
 })
---
+++ src/views/main/main.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        {{test}}
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            test:'something'
+        }
+    }
+}
+</script>
+
+<style lang=less>
+
+</style>
