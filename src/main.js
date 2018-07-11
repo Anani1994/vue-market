1614ddeff9e78edea49792a7299b1754dd374b60
Anani1994 <dongwhchn@163.com>
2018-07-12 01:15:39 +0800

    add iView

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue';
+import iView from 'iview';
 import App from './app.vue';
 import { router } from './router/index';
+import 'iview/dist/styles/iview.css';
+
+Vue.use(iView);
 
 new Vue({ // 创建 vue 实例
   // 提供一个在页面上已经存在的 DOM 元素作为 Vue 实例挂载目标
