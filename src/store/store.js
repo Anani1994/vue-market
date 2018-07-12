cb952cdab3e2ecd77dcdb82a8504813496dea416
Anani1994 <dongwhchn@163.com>
2018-07-12 20:41:16 +0800

    set vuex

---
+++ src/store/store.js
@@ -0,0 +1,24 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+    state: {
+        count: 1
+    },
+    getters: {
+        //
+    },
+    mutations: {
+        //
+    },
+    actions: {
+        //
+    },
+    modules: {
+        //
+    }
+});
+
+export default store;
